a1a83254c9f2515a72dd6139160358f11d047a6f
Tamal <ckbtamaldipnew@gmail.com>
2025-09-08 21:52:58 +0600

    Add file upload system

--- server/index.js
+++ server/index.js
@@ -2,7 +2,17 @@ import express from 'express'
 import cors from 'cors'
 import multer from 'multer'
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, `${uniqueSuffix}-${file.originalname}`);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 const app = express();
 
