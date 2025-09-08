01fbe8ed45e032b65a3ebfd31bf7e9245f86bd12
Tamal <ckbtamaldipnew@gmail.com>
2025-09-08 21:37:18 +0600

    backend -> add multer setup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 import express from 'express'
 import cors from 'cors'
+import multer from 'multer'
+
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
 
 app.use(cors());
 
+//? All routes
 app.get('/', (req, res) => {
     return res.json({ status: "All Good" });
-})
+});
+
+app.post('/upload/pdf', upload.single('pdf'), (req, res) => {
+    return res.json({ message: 'uploaded' });
+});
 
 app.listen(8000, () => console.log("Server started on PORT : 8000"));
