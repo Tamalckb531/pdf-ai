c76146b08481aeb112f08d7b4cfb871809c6f8ac
Tamal <ckbtamaldipnew@gmail.com>
2025-09-10 00:14:24 +0600

    setup worker

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import multer from 'multer'
+import { Queue } from 'bullmq'
+
+const queue = new Queue("file-upload-queue", {
+    connection: {
+        host: 'localhost',
+        port: '6379',
+    }
+});
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -23,7 +31,15 @@ app.get('/', (req, res) => {
     return res.json({ status: "All Good" });
 });
 
-app.post('/upload/pdf', upload.single('pdf'), (req, res) => {
+app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
+    await queue.add(
+        'file-ready',
+        JSON.stringify({
+            filename: req.file.originalname,
+            destination: req.file.destination,
+            path: req.file.path,
+        })
+    )
     return res.json({ message: 'uploaded' });
 });
 
